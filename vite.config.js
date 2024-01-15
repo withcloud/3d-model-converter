c1ce8573aae06106a709aa810f7807d47cf6cb9b
JosephPeng1014 <55421665+JosephPeng1014@users.noreply.github.com>
2024-01-15 15:28:56 +0800

    修正路徑

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,10 @@ export default defineConfig({
   build: {
     rollupOptions: {
       input: {
-        index: "src/index.html",
-        fbx2glb: "src/fbx2glb.html",
-        obj2glb: "src/obj2glb.html",
-        stl2glb: "src/stl2glb.html",
+        index: "index.html",
+        fbx2glb: "fbx2glb.html",
+        obj2glb: "obj2glb.html",
+        stl2glb: "stl2glb.html",
       }
     }
   }
